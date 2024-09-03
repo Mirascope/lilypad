5a0462ac23a2e1be2bc344c024fad99a51fd7c75
William Bakst <william@mirascope.io>
2024-09-03 11:35:00 -0700

    fix: add dummy test so github actions pass

---
+++ tests/test_pass.py
@@ -0,0 +1,6 @@
+"""Test file for getting tests to pass in github actions while we don't have tests."""
+
+
+def test_pass() -> None:
+    """A dummy test for tests to pass before we implement tests."""
+    pass
