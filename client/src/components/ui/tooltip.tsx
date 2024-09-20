3aa9f04aa0fd2074e636dd3bb9d66d0d3ec7b4f3
Brendan <brendan.kao@sotai.ai>
2024-09-20 10:54:34 -0700

    added lib

--- client/src/components/ui/tooltip.tsx
+++ client/src/components/ui/tooltip.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { cn } from '@/lib/utils';
-import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { cn } from "@/lib/utils";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 const TooltipProvider = TooltipPrimitive.Provider;
 
