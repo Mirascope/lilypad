d1b1e497acbe20adf3eadfe22efc3bfb8b09555c
Brendan <brendan.kao@sotai.ai>
2024-12-06 09:54:53 -0800

    feat: removed export

--- lilypad/cli/commands/__init__.py
+++ lilypad/cli/commands/__init__.py
@@ -1,7 +1,6 @@
 """Commands for the `lilypad` CLI."""
 
-from ._utils import get_and_create_config
 from .auth import auth_command
 from .local import local_command
 
-__all__ = ["auth_command", "get_and_create_config", "local_command"]
+__all__ = ["auth_command", "local_command"]
