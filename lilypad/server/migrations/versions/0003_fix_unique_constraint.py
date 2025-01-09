9402c4b62f42a0b401788b4c91024cf3aa3811b9
Brendan <brendan.kao@sotai.ai>
2025-01-09 14:41:18 -0800

    nit: bump migration from 0002 to 0003

--- lilypad/server/migrations/versions/0003_fix_unique_constraint.py
+++ lilypad/server/migrations/versions/0003_fix_unique_constraint.py
@@ -1,7 +1,7 @@
 """fix unique constraint
 
-Revision ID: 0002
-Revises: 0001
+Revision ID: 0003
+Revises: 0002
 Create Date: 2025-01-09 10:30:27.633575
 
 """
@@ -11,8 +11,8 @@ from collections.abc import Sequence
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "0002"
-down_revision: str | None = "0001"
+revision: str = "0003"
+down_revision: str | None = "0002"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
