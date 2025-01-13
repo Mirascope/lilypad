6db3febca03f62ba10008921b9f8b6540e151cc2
Brendan <brendan.kao@sotai.ai>
2025-01-13 14:20:20 -0800

    feat: support both local and prod

--- lilypad/server/main.py
+++ lilypad/server/main.py
@@ -5,11 +5,15 @@ import subprocess
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.responses import Response
+from starlette.types import Scope as StarletteScope
 
 from .api import v0_api
 from .settings import get_settings
@@ -73,4 +77,27 @@ async def validation_exception_handler(
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+
+class SPAStaticFiles(StaticFiles):
+    """Serve the index.html file for all routes."""
+
+    async def get_response(self, path: str, scope: StarletteScope) -> Response:
+        """Get the response for the given path."""
+        try:
+            return await super().get_response(path, scope)
+        except (HTTPException, StarletteHTTPException) as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
+
+
+if settings.environment == "local":
+    app.mount("/", SPAStaticFiles(directory="static", html=True), name="app")
+    app.mount(
+        "/assets",
+        SPAStaticFiles(directory="static/assets", html=True),
+        name="app_assets",
     )
