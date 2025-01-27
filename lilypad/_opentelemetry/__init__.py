a527272aa4cc0c27fe596ecb98f65c3dcf84d5d5
William Bakst <william@mirascope.io>
2025-01-27 15:11:39 -0800

    fix: import suppression issue for opentelemetry imports

--- lilypad/_opentelemetry/__init__.py
+++ lilypad/_opentelemetry/__init__.py
@@ -2,11 +2,23 @@ from contextlib import suppress
 
 with suppress(ImportError):
     from ._opentelemetry_anthropic import AnthropicInstrumentor
+
+with suppress(ImportError):
     from ._opentelemetry_bedrock import BedrockInstrumentor
+
+with suppress(ImportError):
     from ._opentelemetry_google_generative_ai import GoogleGenerativeAIInstrumentor
+
+with suppress(ImportError):
     from ._opentelemetry_mistral import MistralInstrumentor
+
+with suppress(ImportError):
     from ._opentelemetry_openai import OpenAIInstrumentor
+
+with suppress(ImportError):
     from ._opentelemetry_outlines import OutlinesInstrumentor
+
+with suppress(ImportError):
     from ._opentelemetry_vertex import VertexAIInstrumentor
 
 __all__ = [
