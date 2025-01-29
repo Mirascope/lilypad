19a2cc5f8cf4e4b631e4e45b0c03d1bdf25baae3
Koudai Aono <koudai@mirascope.io>
2025-01-29 09:40:43 +0900

    fix: remove unused model

--- lilypad/ee/server/api/v0/main.py
+++ lilypad/ee/server/api/v0/main.py
@@ -1,18 +1,9 @@
 """The `/ee/server/api/v0` FastAPI sub-app for `lilypad`."""
 
-from pydantic import BaseModel
 
 from .....server.api.v0 import api
 from .datasets_api import datasets_router
 
 api.include_router(datasets_router)
-
-
-class SettingsPublic(BaseModel):
-    remote_client_url: str
-    remote_api_url: str
-    github_client_id: str
-    environment: str
-
 
 __all__ = ["api"]
