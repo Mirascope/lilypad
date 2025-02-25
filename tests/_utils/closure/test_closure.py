cb186619208512fe3b81169cc037ddaa5264c72a
Brendan <brendan.kao@sotai.ai>
2025-02-25 10:37:56 -0800

    fix: update test to reflect removal of oxen

--- tests/_utils/closure/test_closure.py
+++ tests/_utils/closure/test_closure.py
@@ -487,7 +487,7 @@ def test_mirascope_response_model_fn() -> None:
             ],
         },
         "pydantic": {
-            "extras": ["timezone"],
+            "extras": None,
             "version": "2.10.6",
         },
     }
@@ -552,7 +552,7 @@ def test_nested_base_model_definitions() -> None:
             ],
         },
         "pydantic": {
-            "extras": ["timezone"],
+            "extras": None,
             "version": "2.10.6",
         },
     }
@@ -576,7 +576,7 @@ def test_nested_handle_issue_method() -> None:
             ],
         },
         "pydantic": {
-            "extras": ["timezone"],
+            "extras": None,
             "version": "2.10.6",
         },
     }
