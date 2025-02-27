fdcf246c39ef9c86d1110fac38b7b1f0a8648c51
Koudai Aono <koudai@mirascope.io>
2025-02-27 09:57:25 +0900

    fix: improve naming

--- lilypad/call_response_chunk.py
+++
@@ -1,7 +0,0 @@
-"""The lilypad `CallResponseChunk` class."""
-
-from mirascope.llm.call_response_chunk import CallResponseChunk as _CallResponseChunk
-
-
-class CallResponseChunk(_CallResponseChunk):
-    """The lilypad `CallResponseChunk` class."""
---
+++ lilypad/message_chunk.py
@@ -0,0 +1,7 @@
+"""The lilypad `CallResponseChunk` class."""
+
+from mirascope.llm.call_response_chunk import CallResponseChunk
+
+
+class MessageChunk(CallResponseChunk):
+    """The lilypad `MessageChunk` class."""
