b81df67dd1df8fb08bb03698aecdf1f3cb9c7da3
Koudai Aono <koudai@mirascope.io>
2025-03-06 18:03:38 +0900

    fix:  add stub generation command

--- lilypad/cli/main.py
+++ lilypad/cli/main.py
@@ -6,6 +6,7 @@ from rich import print
 from typer import Typer
 
 from .commands import local_command
+from .commands.stub import stubs_command
 
 app = Typer()
 
@@ -13,3 +14,7 @@ app.command(name="version", help="Show the Lilypad version.")(
     lambda: print(importlib.metadata.version("python-lilypad"))
 )
 app.command(name="local", help="Run Lilypad Locally")(local_command)
+app.command(
+    "generate-version-stubs",
+    help="Scan the specified module directory and generate stub files for version assignments.",
+)(stubs_command)
