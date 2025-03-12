8039bfc9f48491af1feca0370b825717e18e2fb8
Brendan <brendan.kao@sotai.ai>
2025-03-11 22:11:46 -0700

    fix: tests

--- lilypad/_utils/__init__.py
+++ lilypad/_utils/__init__.py
@@ -4,7 +4,7 @@ from .call_safely import call_safely
 from .closure import Closure, DependencyInfo, get_qualified_name
 from .config import load_config
 from .fn_is_async import fn_is_async
-from .functions import ArgTypes, ArgValues, create_mirascope_call, inspect_arguments
+from .functions import ArgTypes, ArgValues, inspect_arguments
 from .middleware import create_mirascope_middleware, encode_gemini_part
 
 __all__ = [
