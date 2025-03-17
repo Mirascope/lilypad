ed7beb597ee2e512135550e456a303f85de5821e
William Bakst <william@mirascope.io>
2025-03-17 09:50:41 -0700

    fix: issue with azure lazy import in configure

--- lilypad/_configure.py
+++ lilypad/_configure.py
@@ -163,7 +163,10 @@ def configure(
         from lilypad._opentelemetry import AnthropicInstrumentor
 
         AnthropicInstrumentor().instrument()
-    if importlib.util.find_spec("azure.ai.inference") is not None:
+    if (
+        importlib.util.find_spec("azure") is not None
+        and importlib.util.find_spec("azure.ai.inference") is not None
+    ):
         from lilypad._opentelemetry import AzureInstrumentor
 
         AzureInstrumentor().instrument()
