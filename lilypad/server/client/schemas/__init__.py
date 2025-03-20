f9fb7e8783cec3473bba23ee3b24552b62323809
Brendan <brendan.kao@sotai.ai>
2025-03-19 17:04:40 -0700

    nit: ran generate python

--- lilypad/server/client/schemas/__init__.py
+++ lilypad/server/client/schemas/__init__.py
@@ -1,6 +1,5 @@
 """The Schema models for the Lilypad API."""
 
-from .ee_v0 import LicenseInfo, Provider, Tier
 from .v0 import (
     AnnotationTable,
     CommonCallParams,
@@ -9,11 +8,14 @@ from .v0 import (
     GenerationCreate,
     GenerationPublic,
     Label,
+    LicenseInfo,
     OrganizationPublic,
     ProjectPublic,
+    Provider,
     Scope,
     SpanPublic,
     SpanType,
+    Tier,
 )
 
 __all__ = [
