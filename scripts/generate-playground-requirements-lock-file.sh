211e958fcdc2f9b765e2b22c68c351487538daf9
Koudai Aono <koudai@mirascope.io>
2025-03-28 04:23:54 +0900

    fix: change limit

--- scripts/generate-playground-requirements-lock-file.sh
+++ scripts/generate-playground-requirements-lock-file.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/.."
 
-uv pip compile playground-requirements.txt -o playground-requirements.lock --generate-hashes --python-version  3.10
+uv pip compile playground-requirements.txt -o playground-requirements.lock  --python-version  3.10
 
 if [ $? -ne 0 ]; then
     echo "Error generating lock file for playground requirements"
