3d50582856ef3b98c08ba4271c90ef23e0233c2f
William Bakst <99370834+willbakst@users.noreply.github.com>
2025-04-11 09:42:29 -0700

    Update constants.py

--- lilypad/ee/server/constants.py
+++ lilypad/ee/server/constants.py
@@ -1,5 +1,5 @@
 """Constants for Lilypad Enterprise Edition"""
 
 HOST_NAME = "mirascope.com"
-REMOTE_API_URL = "https://api.mirascope.com/lilypad"
+REMOTE_API_URL = "https://lilypad-production.up.railway.app"
 REMOTE_CLIENT_URL = "https://lilypad.mirascope.com"
