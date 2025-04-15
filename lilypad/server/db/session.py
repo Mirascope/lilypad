15ce7d46bff8dbef6f4b38d57ea26432e381a05b
Brendan <brendan.kao@sotai.ai>
2025-04-15 09:37:02 -0700

    wip: revert

--- lilypad/server/db/session.py
+++ lilypad/server/db/session.py
@@ -44,11 +44,7 @@ class DatabaseEngine:
                 )
             else:
                 self._engine = create_engine(
-                    database_url,
-                    pool_size=16,
-                    max_overflow=8,
-                    pool_pre_ping=True,
-                    pool_recycle=1800,
+                    database_url, pool_size=5, max_overflow=10, pool_pre_ping=True
                 )
         return self._engine
 
