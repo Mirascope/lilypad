32ebcd7dab4ca579441690b8d32ffda632dd94c9
William Bakst <william@mirascope.io>
2025-05-12 23:48:57 -0700

    chore: remove stale docs

--- docs/pages/guides/_meta.tsx
+++
@@ -1,9 +0,0 @@
-import { DocsMenuSeparator } from "@/components/DocsMenuSeparator";
-
-export default {
-    "-- Docs Menu Separator": {
-      type: "separator",
-      title: <DocsMenuSeparator />,
-    },
-    index: "Overview",
-};
