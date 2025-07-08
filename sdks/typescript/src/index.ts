fc26e7ed0bf62c51f5c5e2894eb66b9bda37d791
Koudai Aono <koudai@mirascope.io>
2025-07-08 23:09:55 +0900

    refactor: remove unused exports and streamline index.ts

--- sdks/typescript/src/index.ts
+++ sdks/typescript/src/index.ts
@@ -4,46 +4,18 @@
  * LLM observability and monitoring for TypeScript/JavaScript applications
  */
 
-import { configure, getTracerProvider, getTracer } from './configure';
+import { configure, getTracer } from './configure';
 import { shutdown } from './shutdown';
-import { Span, span, syncSpan } from './span';
-import {
-  session,
-  sessionAsync,
-  withSession,
-  withSessionAsync,
-  getCurrentSession,
-  generateSessionId,
-} from './session';
 import { logger } from './utils/logger';
 
 export { configure, getTracerProvider, getTracer } from './configure';
 export { shutdown } from './shutdown';
-export { Span, span, syncSpan } from './span';
-export {
-  session,
-  sessionAsync,
-  withSession,
-  withSessionAsync,
-  getCurrentSession,
-  generateSessionId,
-} from './session';
 export { logger } from './utils/logger';
 export type { LilypadConfig, LogLevel } from './types';
 
 const lilypad = {
   configure,
   shutdown,
-  Span,
-  span,
-  syncSpan,
-  session,
-  sessionAsync,
-  withSession,
-  withSessionAsync,
-  getCurrentSession,
-  generateSessionId,
-  getTracerProvider,
   getTracer,
   logger,
 };
