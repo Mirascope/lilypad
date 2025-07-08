15a68f593aec0cf87ed303e7ccc3092ebd777e7d
Koudai Aono <koudai@mirascope.io>
2025-07-08 20:10:52 +0900

    feat: add span and session functionality with type definitions and validation constants

--- sdks/typescript/src/index.ts
+++ sdks/typescript/src/index.ts
@@ -8,12 +8,17 @@ import { configure, getTracerProvider, getTracer } from './configure';
 import { shutdown } from './shutdown';
 import { traceOpenAICompletion, createSpan, getActiveSpan, withSpan } from './trace';
 import { logger } from './utils/logger';
+import { Span, span, syncSpan } from './span';
+import { session, sessionAsync, getCurrentSession, withSession, withSessionAsync } from './session';
 
 export { configure, getTracerProvider, getTracer } from './configure';
 export { shutdown } from './shutdown';
 export { traceOpenAICompletion, createSpan, getActiveSpan, withSpan } from './trace';
 export { logger } from './utils/logger';
+export { Span, span, syncSpan } from './span';
+export { session, sessionAsync, getCurrentSession, withSession, withSessionAsync } from './session';
 export type { LilypadConfig, LogLevel } from './types';
+export type { Session } from './session';
 
 const lilypad = {
   configure,
@@ -25,6 +30,16 @@ const lilypad = {
   getTracerProvider,
   getTracer,
   logger,
+  // New span functionality
+  Span,
+  span,
+  syncSpan,
+  // Session functionality
+  session,
+  sessionAsync,
+  getCurrentSession,
+  withSession,
+  withSessionAsync,
 };
 
 export default lilypad;
